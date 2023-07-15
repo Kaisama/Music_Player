6b865955744f0e3afd6354a1d7716a74778fd814
Akshat Bisht <109125241+Kaisama@users.noreply.github.com>
2023-07-15 10:34:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Music_Player
+# Music_player
---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "love yourself",
+    artist: "justin bieber",
+    img: "music-7",
+    src: "music-7"
+  },
+   {
+    name: "Janiye",
+    artist: "Noor Chahal",
+    img: "music-8",
+    src: "music-8"
+  },
+   {
+    name: "My person",
+    artist: "spencer crandall",
+    img: "music-9",
+    src: "music-9"
+  },
+];
